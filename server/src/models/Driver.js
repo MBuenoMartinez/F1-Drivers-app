b68fbbf04f5d9b71c882ed960bf329e920eb64d8
MBuenoMartinez <martin.bueno10@hotmail.com>
2023-10-23 11:56:28 -0300

    Creacion de modelos

---
+++ server/src/models/Driver.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Driver", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    //  dob = day of birth
+    dob: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
