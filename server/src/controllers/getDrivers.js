7295fe9fb91183dece0fe62028be59249255d86f
MBuenoMartinez <martin.bueno10@hotmail.com>
2023-10-24 21:02:11 -0300

    Ruta get/name configurada

--- server/src/controllers/getDrivers.js
+++ server/src/controllers/getDrivers.js
@@ -2,6 +2,7 @@ const { Driver } = require("../db");
 const getDrivers = async (req, res) => {
   try {
     const drivers = await Driver.findAll();
+
     return res.status(200).json(drivers);
   } catch (error) {
     return res.status(500).json({ error: error.message });
--- server/src/controllers/getDriversByName.js
+++ server/src/controllers/getDriversByName.js
@@ -1,12 +1,12 @@
 const { Driver } = require("../db");
-
 const { Op } = require("sequelize");
 
 const getDriversByName = async (req, res) => {
   try {
     const { name } = req.query;
+
     if (!name) {
-      return res.status(400).send("Debes proporcionar un nombre del driver.");
+      return res.status(400).send("Debes proporcionar un nombre de driver.");
     }
 
     const drivers = await Driver.findAll({
@@ -18,13 +18,15 @@ const getDriversByName = async (req, res) => {
     });
 
     if (drivers.length === 0) {
-      return res.status(404).json({ message: "No se encontraron drivers." });
+      return res
+        .status(404)
+        .json({ message: "No se encontraron drivers con ese nombre." });
     }
 
     res.status(200).json(drivers);
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
+
 module.exports = getDriversByName;
